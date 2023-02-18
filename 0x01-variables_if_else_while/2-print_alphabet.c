eb68126792077eae6a6f75535b6fc9523225c25c
ginonno <ginonnosnr1@gmai.com>
2023-02-18 00:50:05 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -print the alphabets in lower case
+ *
+ * Return: 0
+*/
+int main(void)
+	char ch;
+	for (ch = 'a';ch <='z'; ch++)
+	putchar(ch);
+	putchar("\n');
+	return (0);
