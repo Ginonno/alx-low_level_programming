88a00f389f308afe1f76ebf2370305d4b95e9992
ginonno <ginonnosnr1@gmai.com>
2023-02-18 05:47:37 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main -m function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		prinf("%d", i);
+	printf("\n");
+	return (0);
+}
