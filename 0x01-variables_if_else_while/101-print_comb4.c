02285b14ebfeb957a648bf4310aa05ea70fb055a
ginonno <ginonnosnr1@gmai.com>
2023-02-19 02:46:38 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,8 +23,10 @@ int main(void)
 					putchar(a + '0');
 					putchar(b + '0');
 					putchar(c + '0');
-					putchar(',');
-					putchar(' ');
+					if (a + b + C != 24)
+					{
+						putchar(',');
+						putchar(' ');
 				}
 			}
 		}
