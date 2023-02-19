ed926983c3f7c304e7891af30437bc888d00c302
ginonno <ginonnosnr1@gmai.com>
2023-02-19 05:29:59 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,6 @@ int main(void)
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(',');
-			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 			if (i != 99 || j != 99)
