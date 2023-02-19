0ae6feb02cef18d97648af61167deedaf44224fc
ginonno <ginonnosnr1@gmai.com>
2023-02-19 03:52:10 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 /**
- * main -main function
+  * main - main function
  *
  * Return: always 0
  */
-
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-
+     	int a;
+       	int b;
+       	int c;
+       
 	for (a = 0 ; a < 7 ; a++)
 	{
 		for (b = 1 ; b < 8 ; b++)
 		{
-			for (c = 2 ; c < 9 ; c++)
+		       	for (c = 2 ; c < 9 ; c++)
 			{
-				if (a < b && b < c)
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(c + '0');
-					if (a + b + c != 24)
-					{
-						continue;
-					}
-						putchar(',');
+			       	if (a < b && b < c)
+			       	{
+				       	putchar(a + '0');
+                   		        putchar(b + '0');
+                   		        putchar(c + '0');
+					if (a + b + c == 24)
+				       	{
+					       	putchar(',');
 						putchar(' ');
+					}
 				}
 			}
 		}
-
-	putchar('\n');
-
-	return (0);
+	      	putchar('\n');
+	       	return (0);
 }
+
